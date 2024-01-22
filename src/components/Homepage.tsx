d566fa8dd8d3e363d0a2ae3f7308468aa4e5449e
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-01-22 13:56:13 -0500

    Create new layout for homepage.

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,18 +1,26 @@
-import Search from "./Search";
 import "../styles/css/Homepage.css";
 import { useState } from "react";
+import Search from "./Search";
+import About from "./About";
+import Featured from "./Featured";
+import Anchor from "./Anchor";
+import { HomepageProps } from "../types/types";
 
-const Homepage = () => {
+const Homepage = (props: HomepageProps) => {
   const [searchTerm, setSearchTerm] = useState("");
+
+  document.body.style.backgroundImage = `url(./OpenEditions_Homepage.jpg)`;
+  document.body.style.backgroundPosition = `5% 100%`;
+  document.body.style.backgroundSize = "100%";
 
   return (
     <>
-      <header>
+      <header id="homepage-header">
         <div id="site-title">
           <h1>Open Editions</h1>
           <h2>A new way to listen.</h2>
         </div>
-        <nav>
+        <nav id="homepage-nav">
           <input
             id="search-bar"
             value={searchTerm}
@@ -23,13 +31,37 @@ const Homepage = () => {
           <i className="bi bi-search" />
         </nav>
       </header>
-      <main>
-        <section id="about">Who we are</section>
-        <section id="search">
-          <Search term={searchTerm} />
-        </section>
+      <main id="homepage-main">
+        <About />
+        <Anchor id="featured" />
+        <Featured {...props} />
+        <Search term={searchTerm} />
         <section id="contact"></section>
       </main>
+      <footer id="homepage-footer">
+        <section className="homepage-footer-left">
+          <p>
+            Powered by{" "}
+            <a
+              href="https://librivox.org/"
+              target="_blank"
+            >
+              Librivox
+            </a>
+            .
+          </p>
+        </section>
+        <section className="homepage-footer-right">
+          <p>Site design and coding by</p>
+          <a
+            id="homepage-portfolio-link"
+            href="https://rebeccashoptaw.dev/"
+            target="_blank"
+          >
+            Rebecca Shoptaw
+          </a>
+        </section>
+      </footer>
     </>
   );
 };
